9f377d18e80d3afffcc91f74ae4ad15f7bff5dfd
Rajneesh Shukla <44999300+rajneesh18@users.noreply.github.com>
2019-12-08 00:18:47 +0530

    Update singleLinkedList.js

--- Linked List/singleLinkedList.js
+++ Linked List/singleLinkedList.js
@@ -1,4 +1,5 @@
-/** Single Linked List
+/** 
+ * Script: Single Linked List
  * Linked list is a collection of nodes, which is linked to each other by the address
  * of another node stored in the node object.
 */
@@ -19,33 +20,17 @@ class LinkedList {
     }
 
     // Function to check list is empty or not
-    isEmpty = () => {
-        if(this.head == null)
-            return true;
-        return false;
-    }
+    isEmpty = () => { return (this.head) ? false : true ; }
 
     // Function to get the size of the list
-    getSize = () => {
-        if(this.isEmpty())
-            return 0;
-        else {
-            let current = this.head;
-            let size = 1;
-            while (current.next != null) {
-                current = current.next;
-                size++;
-            }
-            return size;
-        }
-    }
+    getSize = () => { return this.size; }
 
     // Function to print the list
     show = () => {
         if(!this.isEmpty()) {
             let current = this.head;
             let str = "[ ";
-            str += `${current.data}`;
+            str += current.data;
             while(current.next != null) {
                 current = current.next;
                 str += `, ${current.data}`;
@@ -61,18 +46,23 @@ class LinkedList {
         // New Node is created with property data and next = null
         let newNode = new Node(data);
         
+        // Check the list is empty or not
         if(this.isEmpty())
             this.head = newNode;
         else {
             newNode.next = this.head;
             this.head = newNode;
         }
+        this.size++;    // Increment the size of the list
+
+        return this.head;
     }
 
     // Insert the element at the end of the list
     addLast = (data) => {
         let newNode = new Node(data);
 
+        // Check the list is empty or not
         if(this.isEmpty())
             this.head = newNode;
         else {
@@ -82,14 +72,132 @@ class LinkedList {
             }
             current.next = newNode;
         }
+        this.size++;    // Increment the size of the list
 
+        return this.head;
+    }
+
+    // Insert element at any position in the list
+    add = (index, data) => {
+
+        if(index == null || index == undefined) {
+            throw "Index is required"; return;
+        } else  if(data == null || data == undefined) {
+            throw "Data is required"; return;
+        }
+
+        let newNode = new Node(data);
+        if(this.isEmpty() || index < 0) {
+            throw "List is Empty..."; return;
+        }
+
+        if(index == 0 ) {
+            newNode.next = this.head;
+            this.head = newNode;
+
+        } else {
+            let currentIndex = 0;
+            let current = this.head;
+            while((currentIndex < (index-1)) && (current.next != null)){
+                current = current.next;
+                currentIndex++;
+            }
+            newNode.next = current.next;
+            current.next = newNode;
+        }
+        this.size++;    // Increment the size of the list.
+
+        return this.head;
+    }
+
+    // Remove first element from list
+    removeFirst = () => {
+        if(this.isEmpty()) return;
+        let temp = this.head;
+        this.head = this.head.next;
+
+        this.size--;    // Decrement the size of the list
+
+        return this.head;
+    }
+
+    // Remove last element from list
+    removeLast = () => {
+        if(this.isEmpty()) return;
+        let current = this.head;
+        while(current.next.next != null) {
+            current = current.next;
+        }
+
+        let removedElement = current.next.data;
+        current.next = null;
+
+        this.size--;    // Decrement the size of the list
+
+        return  this.head;
+    }
+
+    // Remove element at any position in the list
+    remove = (index) => {
+
+        // Check if index is present in list
+        if(index < 0 || index >= this.getSize()) { return null; }
+
+        let current = this.head;
+        let previous;
+        let currentIndex = 0;
+
+
+        // Check if element is the first element
+        if (index === 0 ) {
+            this.head = current.next;
+        } else {
+            while( currentIndex < index ) {
+                currentIndex++;
+                previous = current;
+                current = current.next;
+            }
+            previous.next = current.next;
+        }
+
+        this.size--;    // Decrement the size of the list
+        return this.head;
+    }
+
+    elementAt = (index) => {
+        // Index is required
+        if(index == null || index == undefined) { throw "Index is required"; return; }
+
+        // Check if index is present in list
+        if(index < 0 || index >= this.getSize()) { return null; }
+
+        let currentIndex = 0;
+        let current = this.head;
+
+        while(currentIndex < index) {
+            current = current.next;
+            currentIndex++;
+        }
+
+        return current.data;    // return the element at that index
     }
 }
 
 // Linked List Implementation
 let list = new LinkedList();
-list.addLast(5);
-list.addFirst(7);
-list.addLast(3);
-list.addFirst(9);
+
+// Add Elements
+list.addLast('A');
+list.addLast('B');
+list.addFirst('D');
+list.add(2,'E');
+list.addLast('F');
+list.addFirst('C');
+
+// Remove ELements
+list.removeFirst();
+list.removeLast();
+list.remove(2);
+
 console.log(list.show());
+console.log(list.elementAt(1));
